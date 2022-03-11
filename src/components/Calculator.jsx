1892d750670eba4f7501cc666d1524574827c6c2
Luis Rosario <luisprooc@gmail.com>
2022-03-11 16:36:51 -0400

    feat: add inputs

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,55 +1,88 @@
 import { useState } from "react";
 import { Row, Col } from "reactstrap";
 import "./calculator.css";
-import { IconDollar } from './images/icon-dollar.jsx'
+import { ReactComponent as IconDollar } from "./images/icon-dollar.svg";
 
 const Calculator = () => {
-  const [tipAmount, setTipAmount] = useState(0.00.toFixed(2));
-  const [total, setTotal] = useState(0.00.toFixed(2));
+  const [tipAmount, setTipAmount] = useState((0.0).toFixed(2));
+  const [total, setTotal] = useState((0.0).toFixed(2));
+  const [totalPeople, setTotalPeople] = useState(0);
+  const [bill, setBill] = useState(0);
 
   const reset = () => {
-    setTipAmount(0.00.toFixed(2));
-    setTotal(0.00.toFixed(2));
-  }
+    setTipAmount((0.0).toFixed(2));
+    setTotal((0.0).toFixed(2));
+  };
 
   return (
     <Row className="cal-container">
       <Col lg="7">
-        <div>
-          <label>Bill</label> <br/>
-          <input/>  
+        <div className="cal-container-inputs">
+          <label className="cal-label-inputs" for="bill" id="cal-label-bill">
+            Bill
+          </label>{" "}
+          <br />
+          <input className="cal-inputs" placeholder={bill} />
         </div>
-        <div>
-          <label>Select Tip %</label> <br/>
-          <input/>  
+        <div className="cal-container-inputs">
+          <label className="cal-label-inputs">Select Tip %</label> <br />
+          <Row className="g-0">
+            <Col>
+              <button className="cal-tip-btn">5%</button>
+            </Col>
+            <Col>
+              <button className="cal-tip-btn">10%</button>
+            </Col>
+            <Col>
+              <button className="cal-tip-btn">15%</button>
+            </Col>
+            
+          </Row>
         </div>
-        <div>
-          <label>Number of People</label> <br/>
-          <input/>  
+        <div className="cal-container-inputs">
+          <label className="cal-label-inputs">Number of People</label> <br />
+          <input className="cal-inputs" placeholder={totalPeople} />
         </div>
       </Col>
       <Col className="cal-total-box" lg="5">
         <Row className="mb-4">
           <Col>
-            <p>Tip Amount <br /> <span className="cal-span-person">/{" "} person</span></p>
+            <p>
+              Tip Amount <br />{" "}
+              <span className="cal-span-person">/ person</span>
+            </p>
           </Col>
           <Col>
             <div className="test">
-              <p className="cal-total-digits"><span><IconDollar width={35} height={35}/> </span> {tipAmount}</p>
+              <p className="cal-total-digits">
+                <span>
+                  <IconDollar width={35} height={35} />{" "}
+                </span>{" "}
+                {tipAmount}
+              </p>
             </div>
           </Col>
         </Row>
         <Row className="mt-4">
           <Col>
-            <p>Total <br /> <span className="cal-span-person">/{" "} person</span></p>
+            <p>
+              Total <br /> <span className="cal-span-person">/ person</span>
+            </p>
           </Col>
           <Col>
-            <p className="cal-total-digits"><span><IconDollar width={35} height={35}/> </span>  {total}</p>
+            <p className="cal-total-digits">
+              <span>
+                <IconDollar width={35} height={35} />{" "}
+              </span>{" "}
+              {total}
+            </p>
           </Col>
         </Row>
         <Row className="cal-total-row-btn">
           <Col className="mx-auto text-center">
-            <button className="cal-total-btn" onClick={reset}>RESET</button>
+            <button className="cal-total-btn" onClick={reset}>
+              RESET
+            </button>
           </Col>
         </Row>
       </Col>
