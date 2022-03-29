5e3e37bee84da0a9e7842499bf5e1f70f3ee1785
Luis Rosario <luisprooc@gmail.com>
2022-03-29 16:49:10 -0400

    feat: add funcionality

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Row, Col } from "reactstrap";
 import "./calculator.css";
 import { ReactComponent as IconDollar } from "./images/icon-dollar.svg";
@@ -6,51 +6,111 @@ import { ReactComponent as IconDollar } from "./images/icon-dollar.svg";
 const Calculator = () => {
   const [tipAmount, setTipAmount] = useState((0.0).toFixed(2));
   const [total, setTotal] = useState((0.0).toFixed(2));
-  const [totalPeople, setTotalPeople] = useState(0);
-  const [bill, setBill] = useState(0);
+  const [totalPeople, setTotalPeople] = useState("");
+  const [bill, setBill] = useState("");
+  const [tip, setTip] = useState("");
+  const btnRef = useRef(null);
+  const btnValues = [5, 10, 15, 25, 50];
 
   const reset = () => {
+    setBill("");
+    setTip("");
+    setTotalPeople("");
     setTipAmount((0.0).toFixed(2));
     setTotal((0.0).toFixed(2));
   };
+
+  const splitter = () => {
+    const amount = tip / 100;
+    const totalPerPerson = (parseFloat(bill) + parseFloat(bill) * amount) / totalPeople;
+    setTotal(totalPerPerson.toFixed(2));
+
+    const totalTipPerPerson = parseFloat(bill) * amount / totalPeople;
+    setTipAmount(totalTipPerPerson.toFixed(2));
+
+  }
+
+  useEffect(() => {
+    if (bill && tip && totalPeople) {
+      btnRef.current.disabled = false;
+      splitter();
+    }
+
+    // else if(bill && tip && totalPeople == null){
+    //   alert("MMG")
+    // }
+    else {
+      btnRef.current.disabled = true;
+    }
+  }, [tip, bill, totalPeople]);
 
   return (
     <Row className="cal-container">
       <Col lg="6">
         <div className="cal-container-inputs">
-          <label className="cal-label-inputs" for="bill" id="cal-label-bill">
+          <label
+            className="cal-label-inputs"
+            htmlFor="bill"
+            id="cal-label-bill"
+          >
             Bill
           </label>{" "}
           <br />
-          <input className="cal-inputs" placeholder={bill} />
+          <input
+            className="cal-inputs"
+            placeholder={0}
+            onInput={(e) => setBill(e.target.value)}
+            maxLength={22}
+            value={bill}
+          />
         </div>
         <div className="cal-container-inputs">
-          <label className="cal-label-inputs">Select Tip %</label> <br />
+          <label className="cal-label-inputs" htmlFor="tip">
+            Select Tip %
+          </label>{" "}
+          <br />
           <Row>
+            {btnValues.map((value) => (
+              <Col lg="4" key={value}>
+                <button
+                  className={
+                    parseInt(tip) === value
+                      ? "cal-tip-btn mb-3 cal-tip-btn-selected"
+                      : "cal-tip-btn mb-3"
+                  }
+                  onClick={(e) => setTip(parseInt(e.target.value))}
+                  value={value.toString()}
+                >
+                  {value}%
+                </button>
+              </Col>
+            ))}
             <Col lg="4">
-              <button className="cal-tip-btn">5%</button>
+              <input
+                id="cal-tip-input"
+                placeholder="Custom"
+                onInput={(e) => setTip(parseInt(e.target.value))}
+                maxLength={3}
+              />
             </Col>
-            <Col lg="4">
-              <button className="cal-tip-btn">10%</button>
-            </Col>
-            <Col lg="4">
-              <button className="cal-tip-btn">15%</button>
-            </Col>
-            <Col lg="4" className="mt-3">
-              <button className="cal-tip-btn">25%</button>
-            </Col>
-            <Col lg="4" className="mt-3">
-              <button className="cal-tip-btn">50%</button>
-            </Col>
-            <Col lg="4" className="mt-3">
-            <input id="cal-tip-input" placeholder={"Custom"} />
-            </Col>
-            
           </Row>
         </div>
         <div className="cal-container-inputs">
-          <label className="cal-label-inputs" id="cal-label-people" for="people">Number of People</label> <br />
-          <input className="cal-inputs" placeholder={totalPeople} />
+          <label
+            className="cal-label-inputs"
+            id="cal-label-people"
+            htmlFor="people"
+          >
+            Number of People
+          </label>{" "}
+          <br />
+          <input
+            className="cal-inputs"
+            placeholder={0}
+            maxLength={22}
+            onInput={(e) => setTotalPeople(e.target.value)}
+            value={totalPeople}
+          />
         </div>
       </Col>
       <Col className="cal-total-box" lg="6">
@@ -89,7 +149,7 @@ const Calculator = () => {
         </Row>
         <Row className="cal-total-row-btn">
           <Col className="mx-auto text-center">
-            <button className="cal-total-btn" onClick={reset}>
+            <button className="cal-reset-btn" onClick={reset} ref={btnRef}>
               RESET
             </button>
           </Col>
